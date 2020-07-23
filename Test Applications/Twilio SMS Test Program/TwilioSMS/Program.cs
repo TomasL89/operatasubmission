25eff0740bae5c616076805048b416bdb0da5321
TomasL89 <tomas.leitch@gmail.com>
2020-07-23 15:42:25 +1000

    Add files via upload

---
+++ Test Applications/Twilio SMS Test Program/TwilioSMS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwilioSMS
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string accountSid = "your secrete ";
+            const string authToken = "your secret token";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "There is a new Log File",
+                from: new Twilio.Types.PhoneNumber("+123456789"),
+                to: new Twilio.Types.PhoneNumber("+123456789")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
