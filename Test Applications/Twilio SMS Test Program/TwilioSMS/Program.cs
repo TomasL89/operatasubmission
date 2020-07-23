aed072f36410f898c3f877978c7cbf3d91d9476b
TomasL89 <tomas.leitch@gmail.com>
2020-07-23 13:35:39 +1000

    Add files via upload

---
+++ Test Applications/Twilio SMS Test Program/TwilioSMS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwilioSMS
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Find your Account Sid and Token at twilio.com/console
+            // DANGER! This is insecure. See http://twil.io/secure
+            const string accountSid = "AC5cd9bd1e9026ea93f03d536d2d70543d";
+            const string authToken = "c4ef466abbfa55855d0025eca5e86c0c";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is a test message for that potential employer?",
+                from: new Twilio.Types.PhoneNumber("+12055707370"),
+                to: new Twilio.Types.PhoneNumber("+61435911879")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
